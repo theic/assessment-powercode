df0ef90a9d3494dce527ea67866c86cc456f0eab
theic <ic.icreator@gmail.com>
2024-03-27 12:33:45 +0200

    refactor: improve readability

--- src/schema.ts
+++ src/schema.ts
@@ -3,25 +3,25 @@ import { Field } from './field';
 export class Schema<T extends Record<string, any>> {
   private fields: Record<keyof T, Field<any>> = {} as Record<keyof T, Field<any>>;
 
-  field<K extends keyof T>(fieldName: K): Field<T[K]> {
+  public field<K extends keyof T>(fieldName: K): Field<T[K]> {
     if (!this.fields[fieldName]) {
       this.fields[fieldName] = new Field<T[K]>();
     }
     return this.fields[fieldName] as Field<T[K]>;
   }
 
-  validate(data: Partial<T>): { isValid: boolean; errors: Partial<Record<keyof T, string[]>> } {
+  public validate(data: Partial<T>): { isValid: boolean; errors: Partial<Record<keyof T, string[]>> } {
     const errors: Partial<Record<keyof T, string[]>> = {};
-  
+
     for (const fieldName in this.fields) {
       const field = this.fields[fieldName];
       const fieldValue = data[fieldName];
-  
+
       if (field instanceof Field) {
         if (fieldValue === undefined && field.isOptional()) {
           continue;
         }
-  
+
         const fieldErrors = field.validate(fieldValue as T[typeof fieldName]);
         if (fieldErrors.length > 0) {
           errors[fieldName] = fieldErrors;
@@ -34,7 +34,7 @@ export class Schema<T extends Record<string, any>> {
         }
       }
     }
-  
+
     const isValid = Object.keys(errors).length === 0;
     return { isValid, errors };
   }
